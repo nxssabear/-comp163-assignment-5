e16599d63bdf70729bace29ec3b930d87893b929
nxssabear <vanessacgray11@gmail.com>
2025-10-01 21:18:01 -0400

    Update Challenge 1: Collatz sequence - while loop for unknown iterations

--- VanessaG_assignment_5.py
+++ VanessaG_assignment_5.py
@@ -1,11 +1,11 @@
 #Challenge 1: Number Sequence Generator
+print("=== Challenge 1: Collatz Conjecture ===")
 current_number = int(input("Enter starting number: "))
 n = current_number
 step_count = 0
 num_sequence = []
 
 while n > 0 and n != 1:
-    n = int(input())
 
     if n % 2 == 0: #even 
         n = n // 2
@@ -22,7 +22,8 @@ for num in num_sequence:
     print(f" {num}",end="")
 print()
 print(f"Steps: {step_count}")
-print() 
+
+print()
 
 #Challenge 2: Prime Number Checker
 print("=== Challenge 2: Prime Number Checker ===")
